b81c05f3b5a2e63ab42713cf8ed279eb9ecbe3d1
shubham pathak <121822895+shubham13101996@users.noreply.github.com>
2023-01-11 15:27:54 +0530

    get project detail

--- controllers/project_controllers.js
+++ controllers/project_controllers.js
@@ -16,3 +16,23 @@ module.exports.create = async function (req, res) {
     return res.redirect('back');
   }
 };
+
+// find project and display it in the project page
+module.exports.project = async function (req, res) {
+    try {
+      let project = await Project.findById(req.params.id).populate({
+        path: 'issues',
+      });
+      if (project) {
+        return res.render('project_page', {
+          title: 'Project Page',
+          project,
+        });
+      }
+      return res.redirect('back');
+    } catch (err) {
+      console.log(err);
+      return res.redirect('back');
+    }
+  };
+  
--- routes/project.js
+++ routes/project.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/project_controller');
-
+// route to create project 
 router.post('/create', projectController.create);
+
+// route to get the project 
+router.get('/:id', projectController.project);
+
 
 module.exports = router;
